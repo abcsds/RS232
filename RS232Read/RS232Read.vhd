46d16621a65159472f6410a46397f5a558e46998
Alberto Barradas <abcsds@gmail.com>
2015-03-04 23:31:46 -0600

    RS232Read: Added hex27seg function

--- RS232Read/RS232Read.vhd
+++ RS232Read/RS232Read.vhd
@@ -8,11 +8,10 @@ entity RS232Read is
 
     RST     :   in  std_logic;
     CLK     :   in  std_logic;
-    Rx      :   in std_logic;
+    Rx      :   in  std_logic;
     NBaud   :   in  std_logic_vector(3 downto 0);
 
-    Q       :   out  std_logic_vector(7 downto 0);
-    EOR     :   out std_logic
+    S       :   out std_logic_vector(13 downto 0)
    );
 end RS232Read;
 
@@ -20,6 +19,9 @@ architecture moore of RS232Read is
 signal FBaud   :   std_logic;
 signal ENC     :   std_logic;
 signal LDx     :   std_logic;
+signal EOR     :   std_logic;
+signal Q       :   std_logic_vector(7 downto 0);
+
 
 component BaudRateRD is
 	port(
@@ -53,10 +55,23 @@ component RegSerPar is
 	);
 end component;
 
+component hex_7seg is
+    port(
+    RST		:	in	std_logic;
+	CLK		:	in 	std_logic;
+    EOR		:	in	std_logic;
+	Q       :   in std_logic_vector(3 downto 0);
+
+    S       :   out std_logic_vector(6 downto 0)
+    );
+end component;
+
 begin
 
-    U00 : FSMRead port map(RST,CLK,Rx,FBaud,ENC,EOR,LDx);
-    U01 : RegSerPar port map(RST,CLK,Rx,LDx,Q);
-    U02 : BaudRateRD port map(RST,CLK,ENC,NBaud,FBaud);
+    U00     :   FSMRead port map(RST,CLK,Rx,FBaud,ENC,EOR,LDx);
+    U01     :   RegSerPar port map(RST,CLK,Rx,LDx,Q);
+    U02     :   BaudRateRD port map(RST,CLK,ENC,NBaud,FBaud);
+    U03     :   hex_7seg port map(RST,CLK,EOR,Q,S(6 downto 0));
+    U04     :   hex_7seg port map(RST,CLK,EOR,Q,S(13 downto 7));
 
 end moore;
