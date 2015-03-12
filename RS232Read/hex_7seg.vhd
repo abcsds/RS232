d2b2548d7d6865c5749a5785770bd98dd0c11c78
Alberto Barradas <abcsds@gmail.com>
2015-03-11 19:46:04 -0600

    hex7seg to combinational

--- RS232Read/hex_7seg.vhd
+++ RS232Read/hex_7seg.vhd
@@ -15,43 +15,32 @@ entity hex_7seg is
 end hex_7seg;
 
 architecture Tabla of Hex_7seg is
-signal Qp, Qn	:	std_logic_vector(6 downto 0);
 
 begin
 
-    SEC: process(RST,CLK,Qn)
-    begin
-        if(RST= '1')then
-            Qp<= (others=>'0');
-        elsif(CLK'event and CLK='1')then
-            Qp<= Qn;
-        end if;
-    end process SEC;
 
     COMB: process(Q,EOR)
 	begin
-        if(EOR= '1')then
-            Qn<= Qp;
-        else
+
             case Q is
-        		when "0000" => Qn <= NOT("1111110");
-        		when "0001" => Qn <= NOT("0110000");
-        		when "0010" => Qn <= NOT("1101101");
-        		when "0011" => Qn <= NOT("1111001");
-        		when "0100" => Qn <= NOT("0110011");
-        		when "0101" => Qn <= NOT("1011011");
-        		when "0110" => Qn <= NOT("1011111");
-        		when "0111" => Qn <= NOT("1110010");
-        		when "1000" => Qn <= NOT("1111111");
-        		when "1001" => Qn <= NOT("1111011");
-                when "1010" => Qn <= NOT("1110111");
-        		when "1011" => Qn <= NOT("0011111");
-        		when "1100" => Qn <= NOT("1001110");
-        		when "1101" => Qn <= NOT("0111101");
-        		when "1110" => Qn <= NOT("0111101");
-        		when "1111" => Qn <= NOT("1001111");
-        		when others => Qn <= NOT("1000111");
+        		when "0000" => S <= NOT("1111110");
+        		when "0001" => S <= NOT("0110000");
+        		when "0010" => S <= NOT("1101101");
+        		when "0011" => S <= NOT("1111001");
+        		when "0100" => S <= NOT("0110011");
+        		when "0101" => S <= NOT("1011011");
+        		when "0110" => S <= NOT("1011111");
+        		when "0111" => S <= NOT("1110010");
+        		when "1000" => S <= NOT("1111111");
+        		when "1001" => S <= NOT("1111011");
+                when "1010" => S <= NOT("1110111");
+        		when "1011" => S <= NOT("0011111");
+        		when "1100" => S <= NOT("1001110");
+        		when "1101" => S <= NOT("0111101");
+        		when "1110" => S <= NOT("0111101");
+        		when "1111" => S <= NOT("1001111");
+        		when others => S <= NOT("1000111");
     		end case;
-        end if;
+
 	end process COMB;
 end tabla;
